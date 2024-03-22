38354c111b1fae9018f23db4e088d30f3641ce44
Milos Radulovic <milosr@niri-ic.com>
2024-03-22 12:56:10 +0100

    Цреатинг Еффецт

---
+++ src/app/store/counter.effects.ts
@@ -0,0 +1,19 @@
+import {Actions, createEffect, ofType} from '@ngrx/effects';
+import {tap} from 'rxjs';
+
+import {decrement, increment} from './counter.actions';
+
+export class CounterEffects {
+	saveCount = createEffect(
+		() => this.actions$.pipe(
+			ofType(increment, decrement),
+			tap((action): void => {
+				console.log(action);
+				localStorage.setItem('count', action.value.toString());
+			})
+		),
+		{dispatch: false}
+	);
+
+	constructor(private actions$: Actions) {}
+}
